5812730b05716cfbe73509961f30cd994dcf0cff
infgotoinf <infgotoinf@gmail.com>
2025-09-15 01:00:59 +0300

    uwu

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
 
 #include <random>
-#include <filesystem>
-#include <fstream>
+#include <chrono>
 
-
+//=================================================================================
+//      FUNCTIONS
+//=================================================================================
 std::random_device dev;
 std::mt19937 rnd(dev());
 
+// Shuffle
 void shuffleIntArray(const int number, int*& array)
 {
     for (int i = number - 1; i > 0; i--)
@@ -18,6 +20,7 @@ void shuffleIntArray(const int number, int*& array)
     }
 }
 
+// Update
 void updateIntArray(const int number, int*& array, bool do_reverse)
 {
     array = new int[number];
@@ -31,100 +34,220 @@ void updateIntArray(const int number, int*& array, bool do_reverse)
     }
 }
 
-
-
-
-unsigned long long fact(int a)
+// Arraying
+void arrayingArrays(const int number, int*& good_array, int*& bad_array, int*& random_array)
 {
-    unsigned long long ret = 1;
-    for (int i = 2; i <= a; i++)
-    {
-        ret *= i;
-    }
-    return ret;
+    updateIntArray(number, good_array, 0);
+    updateIntArray(number, bad_array, 1);
+    updateIntArray(number, random_array, 1);
+    shuffleIntArray(number, random_array);
 }
 
-// An optimized version of Bubble Sort
+// Debug
+void debugArray(const int number, int*& array)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << array[i] << " ";
+    }
+    std::cout << "... ";
+    for (int i = number - 5; i < number; i++)
+    {
+        std::cout << array[i] << " ";
+    }
+    std::cout << '\n';
+}
+
+//---------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
+
+// Benchmark
+namespace chr = std::chrono;
+void benchmark(const int number, int* good_array, int* bad_array, int* random_array, void (*sort)(const int, int*&))
+{
+    // Best
+    chr::time_point start_time = chr::steady_clock::now();
+    sort(number, good_array);
+    chr::milliseconds time = chr::duration_cast<chr::milliseconds>(chr::steady_clock::now() - start_time);
+    std::cout << "- Best: " << float(time.count()) / 1000 << "s\n";
+
+    // Average
+    start_time = chr::steady_clock::now();
+    sort(number, random_array);
+    time = chr::duration_cast<chr::milliseconds>(chr::steady_clock::now() - start_time);
+    std::cout << "- Average: " << float(time.count()) / 1000 << "s\n";
+
+    // Worst
+    start_time = chr::steady_clock::now();
+    sort(number, bad_array);
+    time = chr::duration_cast<chr::milliseconds>(chr::steady_clock::now() - start_time);
+    std::cout << "- Worst: " << float(time.count()) / 1000 << "s\n\n";
+}
+
+void printBenchmark(const char* name, const int number, int* good_array, int* bad_array, int* random_array, void (*sort)(const int, int*&))
+{
+    arrayingArrays(number, good_array, bad_array, random_array);
+    std::cout << name << " for " << number << " numbers:\n";
+    benchmark(number, good_array, bad_array, random_array, sort);
+}
+
+//---------------------------------------------------------------------------------
+//      SORTS
+//---------------------------------------------------------------------------------
+//// Factorial
+//unsigned long long fact(int a)
+//{
+//    unsigned long long ret = 1;
+//    for (int i = 2; i <= a; i++)
+//    {
+//        ret *= i;
+//    }
+//    return ret;
+//}
+
+// Bogo - факториальная
+//Best: O(1)
+//Average: O(n!)
+//Worst : O(n!)
+bool verifyArrayIsSorted(int*& array, const int number)
+{
+    for (int i = 0; i < number; ++i)
+    {
+        if (array[i] != i)
+            return 0;
+    }
+    return 1;
+}
+
+void bogoSort(const int number, int*& array)
+{
+    while (true) {
+       if (verifyArrayIsSorted(array, number))
+           break;
+       shuffleIntArray(number, array);
+    }
+}
+
+// Selection Sort - экспоненциальная
+//Best: O(n ^ 2)
+//Average: O(n ^ 2)
+//Worst : O(n ^ 2)
+void selectionSort(const int n, int*& arr)
+{
+    for (int i = 0; i < n - 1; ++i)
+    {
+        int min_idx = i;
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        std::swap(arr[i], arr[min_idx]);
+    }
+}
+
+// Bubble Sort - полиминальная
 //Best: O(n)
 //Average: O(n ^ 2)
 //Worst : O(n ^ 2)
-void bubbleSort(int*& arr, int n, int& counter) {
+void bubbleSort(const int n, int*& arr)
+{
     bool swapped;
 
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n - 1; ++i) {
         swapped = false;
-        for (int j = 0; j < n - i - 1; j++) {
+        for (int j = 0; j < n - i - 1; ++j) {
             if (arr[j] > arr[j + 1]) {
                 std::swap(arr[j], arr[j + 1]);
                 swapped = true;
-                counter++;
             }
         }
 
-        // If no two elements were swapped, then break
         if (!swapped)
             break;
     }
 }
 
-// To heapify a subtree rooted with node i
-// which is an index in arr[].
-void heapify(int*& arr, int n, int i, int& counter) {
-    counter++;
-
-    // Initialize largest as root
+// Heapsort - линейный логарифм
+//Best: O(n log(n))
+//Average: O(n log(n))
+//Worst : O(n log(n))
+void heapify(int*& arr, int n, int i)
+{
     int largest = i;
-
-    // left index = 2*i + 1
     int l = 2 * i + 1;
-
-    // right index = 2*i + 2
     int r = 2 * i + 2;
 
-    // If left child is larger than root
     if (l < n && arr[l] > arr[largest])
         largest = l;
 
-    // If right child is larger than largest so far
     if (r < n && arr[r] > arr[largest])
         largest = r;
 
-    // If largest is not root
-    if (largest != i) {
+    if (largest != i)
+    {
         std::swap(arr[i], arr[largest]);
-
-        // Recursively heapify the affected sub-tree
-        heapify(arr, n, largest, counter);
+        heapify(arr, n, largest);
     }
 }
 
-// Main function to do heap sort
-void heapSort(int*& arr, int n, int& counter) {
+void heapSort(const int n, int*& arr)
+{
+    for (int i = n / 2 - 1; i >= 0; --i)
+        heapify(arr, n, i);
 
-    // Build heap (rearrange vector)
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i, counter);
-
-    // One by one extract an element from heap
-    for (int i = n - 1; i > 0; i--) {
-
-        // Move current root to end
+    for (int i = n - 1; i >= 0; --i)
+    {
         std::swap(arr[0], arr[i]);
-
-        // Call max heapify on the reduced heap
-        heapify(arr, i, 0, counter);
+        heapify(arr, i, 0);
     }
 }
 
+// Перебор масива - линейная
+// O(n)
+void forArr(const int n, int*& arr)
+{
+    updateIntArray(n, arr, 0);
+    std::cout << "Перебор массива на " << n << " цифр:\n";
+    chr::time_point start_time = chr::steady_clock::now();
+    for (int i = 0; i < n; ++i) arr[i];
+    chr::milliseconds time = chr::duration_cast<chr::milliseconds>(chr::steady_clock::now() - start_time);
+    std::cout << "- Time: " << float(time.count()) / 1000 << "s\n\n";
+}
+
+// Binary Search - логарифмическая
+//Best: O(1)
+//Average: O(log(n))
+//Worst : O(log(n))
+void binarySearch(const int n, int*& arr) {
+    int low = 0;
+    int high = n;
+    int x = 0;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == x)
+            return;
+
+        if (arr[mid] < x)
+            low = mid + 1;
+
+        else
+            high = mid - 1;
+    }
+}
+
+//=================================================================================
+//  MAIN
+//=================================================================================
 int main()
 {
-    int number_of_numbers = 1000;
-    int* good_numbers;
-    int* bad_numbers;
-    updateIntArray(number_of_numbers, good_numbers, 0);
-    updateIntArray(number_of_numbers, bad_numbers, 1);
-    //shuffleIntArray(number_of_numbers, numbers);
-    
+    setlocale(0, "");
+    int* good_arr;
+    int* bad_arr;
+    int* rand_arr;
+    arrayingArrays(5, good_arr, bad_arr, rand_arr);
+
     //std::ofstream file;
     //file.open("out.txt");
     //
@@ -134,27 +257,27 @@ int main()
     //}
     //
     //file.close();
-    int counter = 0;
-    heapSort(good_numbers, number_of_numbers, counter);
-    std::cout << "Best: " << counter << '\n';
-    counter = 0;
-    heapSort(bad_numbers, number_of_numbers, counter);
-    std::cout << "Worst: " << counter << "\n\n";
 
-    updateIntArray(number_of_numbers, good_numbers, 0);
-    updateIntArray(number_of_numbers, bad_numbers, 1);
+    /*debugArray(number_of_numbers, good_numbers);
+    debugArray(number_of_numbers, bad_numbers);
+    debugArray(number_of_numbers, rand_numbers);*/
 
-    bubbleSort(good_numbers, number_of_numbers, counter);
-    std::cout << "Best: " << counter << '\n';
-    counter = 0;
-    bubbleSort(bad_numbers, number_of_numbers, counter);
-    std::cout << "Worst: " << counter << "\n\n";
+    printBenchmark("Bogosort", 10, good_arr, bad_arr, rand_arr, &bogoSort);
 
-    updateIntArray(number_of_numbers, good_numbers, 0);
-    updateIntArray(number_of_numbers, bad_numbers, 1);
+    printBenchmark("Selection Sort", 10000, good_arr, bad_arr, rand_arr, &selectionSort);
 
-    delete[] good_numbers;
-    delete[] bad_numbers;
+    printBenchmark("Bubble Sort", 10000, good_arr, bad_arr, rand_arr, &bubbleSort);
+
+    printBenchmark("Heap Sort", 1000000, good_arr, bad_arr, rand_arr, &heapSort);
+
+    forArr(1000000000, good_arr);
+
+    printBenchmark("Binary Search", 100000000, good_arr, bad_arr, rand_arr, &binarySearch);
+
+
+    delete[] good_arr;
+    delete[] bad_arr;
+    delete[] rand_arr;
 
     return 0;
 }
